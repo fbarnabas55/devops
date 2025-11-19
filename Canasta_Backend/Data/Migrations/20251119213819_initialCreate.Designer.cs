211012c899bb8b060b4365d4b85d84a508c598d1
fbarnabas55 <fenyvesi.barnabas55@gmail.com>
2025-11-19 22:47:56 +0100

    feat: Add initial migration and update game board UI with enhanced styles and structure

--- Canasta_Backend/Data/Migrations/20251119213819_initialCreate.Designer.cs
+++ Canasta_Backend/Data/Migrations/20251119213819_initialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(KanastaDbContext))]
-    [Migration("20251118004552_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20251119213819_initialCreate")]
+    partial class initialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
